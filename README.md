d711296dbb83e5090e7c956813ca301d9796c641
Соколовский Иван Павлович <ip_sokolovskiy@betboom.org>
2022-11-17 20:13:49 +0400

    Storm Viewer

---
+++ README.md
@@ -0,0 +1,3 @@
+# Storm Viewer App
+
+Hacking with Swift - [Hacking with iOS UIKit](https://www.hackingwithswift.com/read) course - Project 1
---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items where item.hasPrefix("nssl") {
+            pictures.append(item)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        let image = pictures[indexPath.row]
+        cell.textLabel?.text = image
+        cell.imageView?.image = UIImage(named: image)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
